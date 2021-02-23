fb54e5d13e0577f52fe4f36af9908108d15389d5
lele <562282501@qq.com>
2021-02-23 15:29:39 +0800

    使用vant组件库

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,13 @@
 <template>
   <div id="app">
-    <i class="toutiao toutiao-wode"></i>
+    <i class="toutiao toutiao-shipin"></i>
     <router-view />
+    <van-button type="primary">主要按钮</van-button>
   </div>
 </template>
-
+<script>
+export default {
+  name: 'App'
+}
+</script>
 <style lang="less"></style>
