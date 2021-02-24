370d590660beeae99a6c2343cb2d748946641eb3
lele <562282501@qq.com>
2021-02-24 16:52:21 +0800

    封装本地存储模块

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,16 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import { getItem, setItem } from '@/utils/storage'
 Vue.use(Vuex)
 const TOKEN_KEY = 'TOUTIAO_USER'
 export default new Vuex.Store({
   state: {
-    user: JSON.parse(window.localStorage.getItem(TOKEN_KEY))
+    user: getItem(TOKEN_KEY)
   },
   mutations: {
     setUser (state, data) {
       state.user = data
-      window.localStorage.setItem(TOKEN_KEY, JSON.stringify(state.user))
+      setItem(TOKEN_KEY, state.user)
     }
   },
   actions: {
---
+++ src/utils/storage.js
@@ -0,0 +1,22 @@
+// 存
+export const setItem = (key, value) => {
+  if (typeof value === 'object') {
+    value = JSON.stringify(value)
+  }
+  window.localStorage.setItem(key, value)
+}
+
+// 取
+export const getItem = key => {
+  const data = window.localStorage.getItem(key)
+  try {
+    return JSON.parse(data)
+  } catch {
+    return data
+  }
+}
+
+// 删
+export const removeItem = key => {
+  window.localStorage.removeItem(key)
+}
