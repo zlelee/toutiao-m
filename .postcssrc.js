c32555ecdd21cc1545f0c3b3893bcf819cdd72a7
lele <562282501@qq.com>
2021-02-24 09:40:11 +0800

    Axios简单配置

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   plugins: {
-    // vue Cli 已经帮我们配置好了,所以不同配置
+    // vue Cli 已经帮我们配置好了,所以不用配置,再配置一次会警告
     // autoprefixer: {
     //   browsers: ['Android >= 4.0', 'iOS >= 8']
     // },
@@ -8,8 +8,9 @@ module.exports = {
     'postcss-pxtorem': {
       // rootValue: 37.5,
       rootValue ({ file }) {
-        return file.indexOf('vant') !== -1 ? 37.5 : 75
+        return file.includes('vant') ? 37.5 : 75
       },
+      // 代表作用于所有的 CSS 属性
       propList: ['*']
     }
   }
