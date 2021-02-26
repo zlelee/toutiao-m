4a98fdab9b282e9f474cd605ba01eccbfd4bf858
lele <562282501@qq.com>
2021-02-26 15:34:13 +0800

    首页头部导航栏布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,6 +1,17 @@
 <template>
-  <div>
-    首页
+  <div class="home-container">
+    <!-- 导航栏 -->
+    <van-nav-bar class="page-nav-bar">
+      <van-button
+        class="search-btn"
+        slot="title"
+        type="info"
+        size="small"
+        round
+        icon="search"
+      >搜索</van-button>
+    </van-nav-bar>
+    <!-- /导航栏 -->
   </div>
 </template>
 
@@ -20,5 +31,19 @@ export default {
 </script>
 
 <style lang='less' scoped>
-
+.home-container {
+  /deep/.van-nav-bar__title {
+    max-width: unset;
+  }
+  .search-btn {
+    width: 555px;
+    height: 64px;
+    background-color: #5babfb;
+    border: none;
+    font-size: 28px;
+    .van-icon {
+      font-size: 32px;
+    }
+  }
+}
 </style>
