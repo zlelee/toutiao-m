066e8246d02e74dd214c15502903dd709fac6643
lele <562282501@qq.com>
2021-02-26 10:35:23 +0800

    个人中心顶部已登录

--- src/views/my/index.vue
+++ src/views/my/index.vue
@@ -12,6 +12,42 @@
         <span class="text">登录 / 注册</span>
       </div>
     </div>
+    <!-- 已登录头部 -->
+     <!-- 已登录 -->
+    <div class="header user-info">
+      <div class="base-info">
+        <div class="left">
+          <van-image
+            class="avatar"
+            round
+            fit="cover"
+            src="https://img.yzcdn.cn/vant/cat.jpeg"
+          />
+          <span class="name">黑马头条号</span>
+        </div>
+        <div class="right">
+          <van-button size="mini" round>编辑资料</van-button>
+        </div>
+      </div>
+      <div class="data-stats">
+        <div class="data-item">
+          <span class="count">10</span>
+          <span class="text">头条</span>
+        </div>
+        <div class="data-item">
+          <span class="count">10</span>
+          <span class="text">头条</span>
+        </div>
+        <div class="data-item">
+          <span class="count">10</span>
+          <span class="text">头条</span>
+        </div>
+        <div class="data-item">
+          <span class="count">10</span>
+          <span class="text">头条</span>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -55,5 +91,72 @@ export default {
       }
     }
   }
+   .user-info {
+    .base-info {
+      height: 231px;
+      padding: 76px 32px 23px;
+      box-sizing: border-box;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      .left {
+        display: flex;
+        align-items: center;
+        .avatar {
+          width: 132px;
+          height: 132px;
+          border: 4px solid #fff;
+          margin-right: 23px;
+        }
+        .name {
+          font-size: 30px;
+          color: #fff;
+        }
+      }
+    }
+    .data-stats {
+      display: flex;
+      .data-item {
+        height: 130px;
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        color: #fff;
+        .count {
+          font-size: 36px;
+        }
+        .text {
+          font-size: 23px;
+        }
+      }
+    }
+  }
+  .grid-nav {
+    .grid-item {
+      height: 141px;
+      i.toutiao {
+        font-size: 45px;
+      }
+      .toutiao-shoucang {
+        color: #eb5253;
+      }
+      .toutiao-lishi {
+        color: #ff9d1d;
+      }
+      span.text {
+        font-size: 28px;
+      }
+    }
+  }
+  .logout-cell {
+    text-align: center;
+    color: #d86262;
+  }
+  .mb-9 {
+    margin-bottom: 9px;
+  }
 }
+
 </style>
