096625549b1a58873809e09ef5cdc400a7a4e050
lele <562282501@qq.com>
2021-02-26 10:11:45 +0800

    个人中心顶部未登录

--- src/views/my/index.vue
+++ src/views/my/index.vue
@@ -1,24 +1,16 @@
 <template>
-  <div>
-     <div class="my-container">
-        <div class="header">
-          <img
-            class="mobile-img"
-            src="~@/assets/mobile.png"
-            @click="$router.push({
-              name: 'login',
-              query: {
-                redirect: '/my'
-              }
-            })"
-          >
-        </div>
-        <div class="grid-nav"></div>
-        <van-cell title="消息通知" is-link url="" />
-        <van-cell title="实名认证" is-link url="" />
-        <van-cell title="用户反馈" is-link url="" />
-        <van-cell title="小智同学" is-link url="" />
-        <van-cell title="系统设置" is-link url="" />
+  <div class="my-container">
+    <!-- 未登录头部 -->
+    <div class="header not-login">
+      <div class="login-btn" @click="$router.push({
+        name: 'login',
+        query: {
+          redirect: '/my'
+        }
+      })">
+        <img class="mobile-img" src="~@/assets/mobile.png" alt="" />
+        <span class="text">登录 / 注册</span>
+      </div>
     </div>
   </div>
 </template>
@@ -38,16 +30,29 @@ export default {
 
 <style lang='less' scoped>
 .my-container {
-  > .header {
+   .header {
     height: 361px;
-    background: url("~@/assets/banner.png") no-repeat;
+    background: url('~@/assets/banner.png');
     background-size: cover;
+  }
+  .not-login {
     display: flex;
     justify-content: center;
     align-items: center;
-    .mobile-img {
-      width: 132px;
-      height: 132px;
+    .login-btn {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      .mobile-img {
+        width: 132px;
+        height: 132px;
+        margin-bottom: 15px;
+      }
+      .text {
+        font-size: 28px;
+        color: #fff;
+      }
     }
   }
 }
