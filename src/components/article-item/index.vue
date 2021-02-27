805dd13b789891bcdd0432fd9479b3247ff001b1
lele <562282501@qq.com>
2021-02-27 18:00:26 +0800

    样式调整

--- src/components/article-item/index.vue
+++ src/components/article-item/index.vue
@@ -1,17 +1,17 @@
 <template>
-  <div>
+  <div class="article-item">
   <van-cell>
     <div slot="title">{{article.title}}</div>
-    <div slot="label">
+    <div slot="label" class="label-info-wrap">
       <span>{{article.aut_name}}</span>
       <span>{{ article.comm_count }}评论</span>
       <span>{{ article.pubdate }}</span>
     </div>
     <van-image v-if="article.cover.type===1" slot="default" :src="article.cover.images[0]">
     </van-image>
-    <div v-if="article.cover.type===3" slot="label" class="coverWrap">
-     <div v-for="(item,i) in article.cover.images" :key="i">
-       <van-image :src="item" width="100" height="100"></van-image>
+    <div v-if="article.cover.type===3" slot="label" class="cover-wrap">
+     <div v-for="(item,i) in article.cover.images" :key="i"  class="cover-item">
+       <van-image :src="item" class="cover-item-img"></van-image>
      </div>
     </div>
   </van-cell>
@@ -37,5 +37,44 @@ export default {
 </script>
 
 <style lang='less' scoped>
+.article-item {
+  .title {
+    font-size: 32px;
+    color: #3a3a3a;
+  }
 
+  .van-cell__value {
+    flex: unset;
+    width: 232px;
+    height: 146px;
+    padding-left: 25px;
+  }
+
+  .right-cover {
+    width: 232px;
+    height: 146px;
+  }
+
+  .label-info-wrap span {
+    font-size: 22px;
+    color: #b4b4b4;
+    margin-right: 25px;
+  }
+
+  .cover-wrap {
+    display: flex;
+    padding: 30px 0;
+    .cover-item {
+      flex: 1;
+      height: 146px;
+      &:not(:last-child) {
+        padding-right: 4px;
+      }
+      .cover-item-img {
+        width: 100%;
+        height: 146px;
+      }
+    }
+  }
+}
 </style>
