0e978e149a1432af2b333f6d063af7804384fd29
lele <562282501@qq.com>
2021-02-27 17:04:19 +0800

    展示不同的列表项内容

--- src/components/article-item/index.vue
+++ src/components/article-item/index.vue
@@ -1,6 +1,19 @@
 <template>
   <div>
-    article
+  <van-cell :title="article.title">
+    <div slot="label">
+      <span>{{article.aut_name}}</span>
+      <span>{{ article.comm_count }}评论</span>
+      <span>{{ article.pubdate }}</span>
+    </div>
+    <van-image v-if="article.cover.type===1" slot="default" :src="article.cover.images[0]">
+    </van-image>
+    <div v-if="article.cover.type===3" slot="label" class="coverWrap">
+     <div v-for="(item,i) in article.cover.images" :key="i">
+       <van-image :src="item" width="100" height="100"></van-image>
+     </div>
+    </div>
+  </van-cell>
   </div>
 </template>
 
