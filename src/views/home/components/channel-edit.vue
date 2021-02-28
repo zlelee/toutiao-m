c25d4ab207bff40557c8dc9463e099361e94203d
lele <562282501@qq.com>
2021-02-28 11:31:47 +0800

    创建频道编辑组件

--- src/views/home/components/channel-edit.vue
+++ src/views/home/components/channel-edit.vue
@@ -1,25 +1,6 @@
 <template>
   <div class="channel-edit">
-    <van-cell title="我的频道" :border="false">
-      <van-button size="mini" round type="danger" plain>编辑</van-button>
-    </van-cell>
-    <van-grid :gutter="10">
-      <van-grid-item
-        class="channel-item"
-        v-for="value in 8"
-        :key="value"
-        text="文字"
-      />
-    </van-grid>
-    <van-cell title="频道推荐" :border="false"></van-cell>
-    <van-grid :gutter="10">
-      <van-grid-item
-        class="channel-item"
-        v-for="value in 8"
-        :key="value"
-        text="文字"
-      />
-    </van-grid>
+
   </div>
 </template>
 
@@ -36,17 +17,5 @@ export default {
 </script>
 
 <style lang="less" scoped>
-.channel-edit {
-  padding: 85px 0;
-  .channel-item {
-    height: 86px;
-    /deep/ .van-grid-item__content {
-      background-color: #f5f5f6;
-      .van-grid-item__text {
-        color: #222;
-        font-size: 28px;
-      }
-    }
-  }
-}
+
 </style>
