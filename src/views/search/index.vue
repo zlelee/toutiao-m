acd46dffddbd9fefdf572e0d38a5aa644b9de8c7
lele <562282501@qq.com>
2021-03-02 15:17:38 +0800

    点击取消返回

--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -1,19 +1,36 @@
 <template>
-  <div>
-    search
+  <div class="search-container">
+    <!-- 搜索栏》事件监听 -->
+    <form action="/">
+      <van-search
+        v-model="value"
+        show-action
+        placeholder="请输入搜索关键词"
+        @search="onSearch"
+        @cancel="onCancel"
+      />
+    </form>
   </div>
 </template>
 
 <script>
 export default {
-  name: '',
+  name: 'Search',
 
   data () {
     return {
+      value: ''
     }
   },
 
-  methods: {}
+  methods: {
+    onSearch (val) {
+      this.$toast(val)
+    },
+    onCancel () {
+      this.$router.push('/')
+    }
+  }
 }
 </script>
 
