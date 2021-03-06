ed77ff162a4da766861ca493b7686baeedd31a25
lele <562282501@qq.com>
2021-03-06 18:47:54 +0800

    处理头部及当前评论项

--- src/views/article/components/comment-reply.vue
+++ src/views/article/components/comment-reply.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="comment-reply">
     <!-- 导航栏 -->
-    <van-nav-bar :title="`${comment.reply_count? comment.reply_count+'条回复':'暂无回复' }`">
-      <van-icon
-        slot="left"
-        name="cross"
-        @click="$emit('close')"
-      />
+    <van-nav-bar
+      :title="
+        `${comment.reply_count ? comment.reply_count + '条回复' : '暂无回复'}`
+      "
+    >
+      <van-icon slot="left" name="cross" @click="$emit('close')" />
     </van-nav-bar>
     <!-- /导航栏 -->
 
     <!-- 当前评论项 -->
-    <comment-item :comment="comment"/>
+    <comment-item :comment="comment" />
     <!-- /当前评论项 -->
 
     <van-cell title="所有回复" />
@@ -37,13 +37,13 @@ export default {
       required: true
     }
   },
-  data () {
+  data() {
     return {}
   },
   computed: {},
   watch: {},
-  created () {},
-  mounted () {},
+  created() {},
+  mounted() {},
   methods: {}
 }
 </script>
