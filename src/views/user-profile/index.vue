4028a0ba5d92119daba1e14f6980e6cf7e116dc6
lele <562282501@qq.com>
2021-03-07 10:40:57 +0800

    页面布局

--- src/views/user-profile/index.vue
+++ src/views/user-profile/index.vue
@@ -1,6 +1,17 @@
 <template>
-  <div>
-    user
+  <div class="user-profile">
+    <van-nav-bar title="个人信息" class="page-nav-bar" left-arrow @click-left="$router.back()"></van-nav-bar>
+     <van-cell title="头像" is-link>
+      <van-image
+        class="avatar"
+        fit="cover"
+        round
+        src="https://img.yzcdn.cn/vant/cat.jpeg"
+      />
+    </van-cell>
+    <van-cell title="昵称" value="内容" is-link />
+    <van-cell title="性别" value="内容" is-link />
+    <van-cell title="生日" value="内容" is-link />
   </div>
 </template>
 
@@ -18,5 +29,10 @@ export default {
 </script>
 
 <style lang='less' scoped>
-
+.user-profile {
+  .avatar {
+    width: 60px;
+    height: 60px;
+  }
+}
 </style>
