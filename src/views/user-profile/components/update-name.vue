77cd2645b57da7b7278d0f4575c3785da7136701
lele <562282501@qq.com>
2021-03-07 11:29:35 +0800

    展示弹出层

---
+++ src/views/user-profile/components/update-name.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="update-name">
+     <van-nav-bar title="修改昵称" left-text="取消" right-text="完成" @click-left="$emit('close')"></van-nav-bar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UpdateName',
+
+  data () {
+    return {
+    }
+  },
+
+  methods: {}
+}
+</script>
+
+<style lang='less' scoped>
+
+</style>
--- src/views/user-profile/index.vue
+++ src/views/user-profile/index.vue
@@ -1,28 +1,46 @@
 <template>
   <div class="user-profile">
-    <van-nav-bar title="个人信息" class="page-nav-bar" left-arrow @click-left="$router.back()"></van-nav-bar>
-     <van-cell title="头像" is-link>
-      <van-image
-        class="avatar"
-        fit="cover"
-        round
-        :src="user.photo"
-      />
+    <van-nav-bar
+      title="个人信息"
+      class="page-nav-bar"
+      left-arrow
+      @click-left="$router.back()"
+    ></van-nav-bar>
+    <van-cell title="头像" is-link>
+      <van-image class="avatar" fit="cover" round :src="user.photo" />
     </van-cell>
-    <van-cell title="昵称" :value="user.name" is-link />
-    <van-cell title="性别" :value="user.gender===0?'男':'女'" is-link />
+    <van-cell
+      title="昵称"
+      :value="user.name"
+      is-link
+      @click="isUpdateNameShow = true"
+    />
+    <van-cell title="性别" :value="user.gender === 0 ? '男' : '女'" is-link />
     <van-cell title="生日" :value="user.birthday" is-link />
+    <!-- 编辑昵称 -->
+    <van-popup
+      v-model="isUpdateNameShow"
+      position="bottom"
+      style="height: 100%"
+    >
+      <update-name @close="isUpdateNameShow = false" />
+    </van-popup>
+    <!-- /编辑昵称 -->
   </div>
 </template>
 
 <script>
 import { getUserProfile } from '@/api/user'
+import UpdateName from './components/update-name.vue'
 export default {
   name: 'UserProfile',
-
-  data () {
+  components: {
+    UpdateName
+  },
+  data() {
     return {
-      user: {}
+      user: {},
+      isUpdateNameShow: false
     }
   },
   created() {
@@ -41,7 +59,7 @@ export default {
 }
 </script>
 
-<style lang='less' scoped>
+<style lang="less" scoped>
 .user-profile {
   .avatar {
     width: 60px;
